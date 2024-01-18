561b243c793f75103a983bc265e79b880669ec14
Matthew Leering <matthew.leering@directimpactsolutions.com>
2024-01-18 13:18:43 -0500

    Adjust to light/dark of user's OS/browser

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import {createRoot} from "react-dom/client";
 import App from "./app.jsx";
 
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.setAttribute('data-bs-theme', 'dark');
+}
+
 const root = createRoot(document.getElementById("root"))
 
 setInterval(()=>{root.render(<App firstName="Matt" lastName="Leering"/>)}, 1000)
