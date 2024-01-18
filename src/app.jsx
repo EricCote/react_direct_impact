5145d14ccf82b4febc57352a475f22b7f4cd68a9
Matthew Leering <matthew.leering@directimpactsolutions.com>
2024-01-18 11:28:31 -0500

    Shortened Fragment

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,13 @@
-import {Fragment} from "react";
 
 export default function App({firstName, lastName}) {
     return (
-        <Fragment>
+        <>
             <h1 style={{color: 'red'}}>
                 Hello {firstName} {lastName}
             </h1>
             <p>
                 It is {new Date().toLocaleTimeString()}
             </p>
-        </Fragment>
+        </>
     )
 }
