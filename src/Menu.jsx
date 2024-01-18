e864f2f8a2b8cc3cbe4d45d6c8966ba848231f88
Matthew Leering <matthew.leering@directimpactsolutions.com>
2024-01-18 14:21:49 -0500

    Customizations to menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,9 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function BasicExample() {
+function Menu() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
@@ -11,19 +10,9 @@ function BasicExample() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="/home">Home</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/counter">Counter</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
@@ -31,4 +20,4 @@ function BasicExample() {
     );
 }
 
-export default BasicExample;
+export default Menu;
