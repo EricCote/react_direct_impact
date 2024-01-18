828518bfea588f0dedec168bf5a2afa708ae0471
Matthew Leering <matthew.leering@directimpactsolutions.com>
2024-01-18 13:24:37 -0500

    Bootstrap-ifying

---
+++ src/MyButton.jsx
@@ -0,0 +1,19 @@
+import {useState} from "react";
+import {Button} from "react-bootstrap";
+
+let count = 0
+
+export default  function MyButton() {
+    let [count,setCount] = useState(0)
+
+    const render = useRender();
+
+    return <Button onClick={(count+=1)}>The count is: {count}</Button>
+}
+
+function useRender() {
+    const [set] = useState()
+    function render() {
+        set({})
+    }
+}
--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,17 @@
+import MyButton from "./MyButton.jsx";
+import "bootstrap/dist/css/bootstrap.css"
+import {Container} from "react-bootstrap";
 
 export default function App({firstName, lastName}) {
     return (
-        <>
+        <Container>
             <h1 style={{color: 'red'}}>
                 Hello {firstName} {lastName}
             </h1>
             <p>
                 It is {new Date().toLocaleTimeString()}
             </p>
-        </>
+            <MyButton/>
+        </Container>
     )
 }
