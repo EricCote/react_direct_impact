48c8c1f51655369de8a57810623252a97c769f51
Matthew Leering <matthew.leering@directimpactsolutions.com>
2024-01-18 14:16:33 -0500

    Added basic routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,23 @@
 import "bootstrap/dist/css/bootstrap.css"
 import Counter from "./Counter/Counter.jsx";
 import {Container} from "react-bootstrap";
+import Home from "./Home/Home.jsx";
+import About from "./About/About.jsx";
 
 export default function App() {
-    return <Container><Counter firstName="Matt" lastName="Leering"/></Container>
+    return <Container>
+        <Routing route={location.pathname}/>
+    </Container>
+}
+
+function Routing({route}) {
+    switch (route) {
+        case '/':
+        case 'home':
+            return <Home/>
+        case '/about':
+            return <About/>
+        case '/counter':
+            return <Counter firstName="Matt" lastName="Leering"/>
+    }
 }
