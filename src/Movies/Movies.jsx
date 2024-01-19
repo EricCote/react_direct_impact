8156b3375fda019e8cfca2bba721f13fd9b6584e
Matthew Leering <matthew.leering@directimpactsolutions.com>
2024-01-19 13:05:07 -0500

    Genre filter now works

--- src/Movies/Movies.jsx
+++ src/Movies/Movies.jsx
@@ -14,14 +14,6 @@ export default function Movies() {
     };
 
     function getMovies(genre) {
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MjY1NTlmZDM3OTlmOTQ2NmM2NGVhMGRhYTQyMDU0MyIsInN1YiI6IjU1NGI0OWM3OTI1MTQxNDY5YzAwMTk1NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qJWSAVUgQylJnOnzQkRHVT6OwJpX9EmTOB5n7JcLPek'
-            }
-        };
-
         fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=' + genre, options)
             .then(response => response.json())
             .then(json => setMovies(json.results))
@@ -45,7 +37,7 @@ export default function Movies() {
         <>
             <h1>Movies</h1>
 
-            <Form.Select aria-label="Default select example">
+            <Form.Select aria-label="Default select example" onChange={(evt) => {evt.preventDefault();getMovies(evt.currentTarget.value)}}>
                 <option>Choose Genre</option>
                 {genres.map(genre => <option value={genre.id} key={genre.id}>{genre.name}</option>)}
             </Form.Select>
