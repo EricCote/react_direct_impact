4c7971ea0585c537a941f944e57045d946814aed
Matthew Leering <matthew.leering@directimpactsolutions.com>
2024-01-19 09:02:10 -0500

    Starting to introduce react-router, but still a W.I.P.

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,7 @@ import {Container} from "react-bootstrap";
 import Home from "./Home/Home.jsx";
 import About from "./About/About.jsx";
 import Menu from "./Menu.jsx";
-import {useEffect, useState} from "react";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 // DOM should never be called from render
 // DOM should only be called from effect
@@ -12,44 +12,32 @@ import {useEffect, useState} from "react";
 // The effect is called ONLY if at least one item from the array changes
 
 export default function App() {
-    const [page, setPage] = useState(location.hash)
-
-    useEffect(() => {
-        window.addEventListener('hashchange', hashchangeHandler)
-    }, []);
-
-    useEffect(() => {
-        document.querySelector('h1').style.color='red'
-        console.log(`moving to ${page}`)
-        return () => {
-            console.log(`moving out of ${page} (previous instance of page)`)
-        }
-    }, [page]);
-
-    function hashchangeHandler() {
-        setPage(location.hash)
-    }
-
 
     return (
-        <>
+        <RouterProvider router={router}>
             <Menu/>
             <Container className='mt-3'>
-                <Routing route={page}/>
+
             </Container>
-        </>
+        </RouterProvider>
     )
 
 }
 
-function Routing({route}) {
-    switch (route) {
-        case '#':
-        case '#home':
-            return <Home/>
-        case '#about':
-            return <About/>
-        case '#counter':
-            return <Counter firstName="Matt" lastName="Leering"/>
-    }
-}
+let router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>
+    },
+    {
+        path: "/about",
+        element: <About/>
+    },
+    {
+        path: "/counter",
+        element: <Counter firstName="Matt" lastName="Leering"/>
+    },
+    {
+        element: <p>Page Cannot Be Found</p>
+    },
+])
