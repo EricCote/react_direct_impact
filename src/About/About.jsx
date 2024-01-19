57b80c52ff8ad19c7c8bc4fa2eef745e05741a2d
Matthew Leering <matthew.leering@directimpactsolutions.com>
2024-01-19 11:35:21 -0500

    Unordered list of authors on the about us page

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,4 +1,16 @@
 
+const authors=[
+    {id: 1, name: 'Matt'},
+    {id: 2, name: 'Alan'},
+    {id: 3, name: 'Xavier'},
+    {id: 4, name: 'The Rock'},
+]
+
 export default function About() {
-    return <h1>About Page</h1>
+    return (
+        <>
+            <h1>About Page</h1>
+            <p>Authors: </p><ul>{authors.map(author => (<li key={author.id}>{author.name}</li>))}</ul>
+        </>
+    )
 }
