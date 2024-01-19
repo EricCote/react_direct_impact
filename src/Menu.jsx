2f4d0c070b3461aafa35d7c306c02645743050d1
Matthew Leering <matthew.leering@directimpactsolutions.com>
2024-01-19 09:25:13 -0500

    Now using NavLinks in navigation so that it utilizes the history API

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {NavLink} from "react-router-dom";
 
 function Menu() {
     return (
@@ -10,9 +11,9 @@ function Menu() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/home">Home</Nav.Link>
-                        <Nav.Link href="/about">About</Nav.Link>
-                        <Nav.Link href="/counter">Counter</Nav.Link>
+                        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                        <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+                        <Nav.Link as={NavLink} to="/counter">Counter</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
