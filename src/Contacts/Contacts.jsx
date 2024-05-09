bba36217f815613791e3789610fd1fc4b33f5eed
Eric Cote <eric@coteexpert.com>
2024-05-09 12:51:49 -0400

    use TanStack ReactQuery

--- src/Contacts/Contacts.jsx
+++ src/Contacts/Contacts.jsx
@@ -3,12 +3,22 @@ import ContactAPI from './ContactApiAzure';
 import { Button, FormControl, Table } from 'react-bootstrap';
 
 import { Save2, XCircle, PencilFill } from 'react-bootstrap-icons';
+import { useQuery } from '@tanstack/react-query';
 
 export default function Contacts() {
-  const contacts = use(ContactAPI.getAllContacts());
-  const [selectedRow, setSelectedRow] = useState(
-    '5f51485e-c5f8-4aba-8cc4-1912a72ef0ae'
-  );
+  // const contacts = use(ContactAPI.getAllContacts());
+
+  const { data: contacts } = useQuery({
+    queryKey: 'contacts',
+    queryFn: ContactAPI.getAllContacts,
+    options: {
+      cacheTime: 10000,
+      staleTime: 30000,
+      refetchOnWindowFocus: true,
+    },
+  });
+
+  const [selectedRow, setSelectedRow] = useState(null);
 
   return (
     <>
@@ -36,10 +46,10 @@ export default function Contacts() {
                   <FormControl defaultValue={c.email} />
                 </td>
                 <td>
-                  <Button>
+                  <Button size='sm' className='me-2'>
                     <Save2 color='white' size={12} />
                   </Button>
-                  <Button>
+                  <Button size='sm'>
                     <XCircle color='white' size={12} />
                   </Button>
                 </td>
@@ -50,7 +60,13 @@ export default function Contacts() {
                 <td>{c.lastName}</td>
                 <td>{c.email}</td>
                 <td>
-                  <Button>
+                  <Button
+                    size='sm'
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setSelectedRow(c.id);
+                    }}
+                  >
                     <PencilFill color='white' size={12} />
                   </Button>
                 </td>
--- src/app.jsx
+++ src/app.jsx
@@ -15,16 +15,25 @@ import Movies from './Movies/Movies.jsx';
 import Contacts from './Contacts/Contacts.jsx';
 import { lazy, Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const About = lazy(() => import('./About/About.jsx')); // This import statement makes the file get separated from the others when doing npm run build
 
-// DOM should never be called from render
-// DOM should only be called from effect
-// Effects are often run with a dependency array
-// The effect is called ONLY if at least one item from the array changes
+// Configure for all queries
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 function Root() {
